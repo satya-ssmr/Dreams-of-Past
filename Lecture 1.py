e98a3a19bc5e9aa86c267227f6205825cab45082
ssmrxp <ssmr.xperience@gmail.com>
2024-04-06 10:23:43 +0100

    Initial commit

---
+++ Lecture 1.py
@@ -0,0 +1,7 @@
+a = 1 # 01
+b = 3 # 11 
+a & b
+#%%
+Triple quotes (also with '''), allow strings to break over multiple lines.
+Alternatively 
+ is a newline character (	 for tab, \ is a single backslash)
---
+++ README.md
@@ -0,0 +1,2 @@
+# Dreams-of-Past
+ 
